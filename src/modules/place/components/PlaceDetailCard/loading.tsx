315a0c2808170030e17ee408356bf36b217d2f5e
ppipee <ppipee.kj@hotmail.com>
2021-03-19 11:52:57 +0700

    fix: type undefined

--- src/modules/place/components/PlaceDetailCard/loading.tsx
+++ src/modules/place/components/PlaceDetailCard/loading.tsx
@@ -1,27 +1,26 @@
 import React from 'react'
 
-import Gap from 'common/components/Gap'
 import Skeleton from 'common/components/Skeleton'
 import Text from 'common/components/Text'
-import fontSizes from 'common/styles/mixins/fontSizes'
+import useFontSizeResponsive from 'common/styles/hooks/useFontSizeResponsive'
 import spaces from 'common/styles/mixins/spaces'
 
 import { CardDetail, CardTitle } from './styled'
 
 const PlaceDetailLoading = () => {
+	const { detailSize, subDetailSize } = useFontSizeResponsive()
+
 	return (
-		<CardDetail>
-			<Gap $size={spaces(8)} $type="vertical" $justifyContent="space-between">
-				<div>
-					<CardTitle size={fontSizes(16)} margin={`0 ${spaces(4)} 0 0`}>
-						<Skeleton width="90%" />
-					</CardTitle>
-					<Skeleton width="55%" />
-				</div>
-				<Text as="div" size={fontSizes(14)}>
-					<Skeleton width="75%" count={2} />
-				</Text>
-			</Gap>
+		<CardDetail $size={spaces(8)} $type="vertical" $justifyContent="space-between">
+			<div>
+				<CardTitle size={detailSize} margin={`0 ${spaces(4)} 0 0`}>
+					<Skeleton width="90%" />
+				</CardTitle>
+				<Skeleton width="55%" />
+			</div>
+			<Text as="div" size={subDetailSize}>
+				<Skeleton width="75%" count={2} />
+			</Text>
 		</CardDetail>
 	)
 }
